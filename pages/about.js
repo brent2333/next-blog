f48c8b2cae0ef9c533dfa0bddc1983065a78acf5
Brent Lawson <brent.lawson@fleetcor.com>
2023-05-22 18:43:49 -0400

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+
+import Head from 'next/head';
+function AboutPage() {
+    return (
+        <>
+        <Head>
+            <title>About page</title>
+        </Head>
+        <h1>About Page</h1>
+        </>
+    )
+}
+
+export default AboutPage;
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+function HomePage() {
+    return (
+        <>
+        <Head>
+            <title>Home page</title>
+        </Head>
+        <main>
+            <h1>my blog</h1>
+        </main>
+        </>
+    )
+}
+
+export default HomePage;
