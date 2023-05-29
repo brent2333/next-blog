21f0885a9d76cc963db7c6dba8d5336250d330e9
Brent Lawson <brent.lawson@fleetcor.com>
2023-05-29 07:13:54 -0400

    add dark mode switch

---
+++ components/DarkTheme.js
@@ -0,0 +1,13 @@
+function DarkTheme() {
+return (
+<style jsx global>{`
+:root {
+     --background-color: #333;
+     --text-color: #ddd;
+     --link-color: orange;
+ }
+`}</style>
+)
+};
+
+export default DarkTheme;
---
+++ components/ThemeSwitch.js
@@ -0,0 +1,35 @@
+import {useState} from 'react'
+import DarkTheme from './DarkTheme';
+function loadDarkMode() {
+    if (typeof localStorage === 'undefined') {
+        return false;
+    }
+    const val = localStorage.getItem('darkMode');
+    return (val === null) ? false : JSON.parse(val);
+}
+function ThemeSwitch() {
+    const [darkMode, setDarkMode] = useState(loadDarkMode);
+    const handleClick = () => {
+        console.log('handleclick')
+        localStorage.setItem('darkMode', JSON.stringify(!darkMode))
+        setDarkMode(!darkMode)
+    }
+    const text = darkMode ? 'Light Mode' : 'Dark Mode';
+    console.log(text)
+    return (
+        <>
+       <button onClick={handleClick} suppressHydrationWarning>{text}</button> 
+       <style jsx>{`
+        button {
+            background: none;
+            border: none;
+            cursor: pointer;
+            color: inherit;
+        }
+       `}</style>
+       {darkMode && <DarkTheme/>}
+
+       </>
+    )
+}
+export default ThemeSwitch;
